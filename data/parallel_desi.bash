f6415c595ab2727aee53561e9ea5c89f0c8fec1b
Mike Smith <mike.smith@aspiaspace.com>
2024-02-07 13:13:58 +0000

    Fixed to stop hammering poor desi servers

--- data/parallel_desi.bash
+++ data/parallel_desi.bash
@@ -2,4 +2,4 @@
 
 DATA=$(tail -n +2 gz_desi_deep_learning_catalog_friendly.csv)
 
-echo $DATA | xargs -n1024 | parallel --jobs 8 --delimiter ' ' ./get_desi.bash {}
+echo $DATA | xargs -n1024 | parallel --delay 2 --jobs 8 --delimiter ' ' ./get_desi.bash {}
