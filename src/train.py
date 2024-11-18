Fix bug for missing backprop signal

src/train.py
@@ -284,7 +284,7 @@ if __name__ == "__main__":
     
     # wrap model into DDP container
     if ddp:
-        model = DDP(model, device_ids=[ddp_local_rank])
+        model = DDP(model, device_ids=[ddp_local_rank], find_unused_parameters=True)
     
     # helps estimate an arbitrarily accurate loss over either split using many batches
     @torch.no_grad()