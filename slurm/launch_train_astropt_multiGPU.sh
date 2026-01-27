3e5c82907ecc8a4c21ae16a0a546c56204eef7a2
Victor Alonso Rodriguez <victoralonsorodriguez61@gmail.com>
2026-01-27 22:48:38 +0000

    train: launch training - changed normalization to asinh

--- slurm/launch_train_astropt_multiGPU.sh
+++ slurm/launch_train_astropt_multiGPU.sh
@@ -13,30 +13,51 @@ REPO_ROOT="/home/valonso/iac18_mhuertas_shared/valonso/astroPT"
 SCRIPT_DIR=$(dirname "$0")
 
 # Training output directory
-OUT_DIR="logs/astropt_100M_250K_arrow_20260127"
+OUT_DIR="logs/astropt_100M_250K_arrow_20260128"
 echo "Target Output Directory: $OUT_DIR"
 
 # Dataset Directory (Arrow)
 DATA_DIR="/home/valonso/iac18_aasensio_shared/euclid_dr1/processed_data_arrow"
 
-
-#--- PART 1: Model Training ---#
-
-# First part of the training from scratch
-JOB1=$(sbatch --parsable "$SCRIPT_DIR/train_astropt_multiGPU.sh" "$REPO_ROOT" "$OUT_DIR" "$DATA_DIR" scratch)
-echo "[Step 1] Job sent (Train Scratch).  ID: $JOB1"
-
-# Second part of the training from resume
-JOB2=$(sbatch --parsable --dependency=afterany:$JOB1 "$SCRIPT_DIR/train_astropt_multiGPU.sh" "$REPO_ROOT" "$OUT_DIR" "$DATA_DIR" resume)
-echo "[Step 2] Job sent (Train Resume).   ID: $JOB2 (Depends on $JOB1)"
+# Launching scripts
+TRAIN_SCRIPT="$SCRIPT_DIR/train_astropt_multiGPU.sh"
+PLOT_MET_SCRIPT="$SCRIPT_DIR/plot_training_metrics.sh"
+PLOT_IMG_SCRIPT="$SCRIPT_DIR/plot_images_spectra.sh"
 
 
-#--- PART 2: Post Training Analysis ---#
+#--- PART 1: First Training Session ---#
+
+# First part of the training from SCRATCH
+JOB1=$(sbatch --parsable "$TRAIN_SCRIPT" "$REPO_ROOT" "$OUT_DIR" "$DATA_DIR" scratch all)
+echo "[Step 1] Job sent (Train Scratch).            ID: $JOB1"
 
 # Plotting the metrics
-JOB3=$(sbatch --parsable --dependency=afterany:$JOB2 "$SCRIPT_DIR/plot_training_metrics.sh" "$REPO_ROOT" "$OUT_DIR")
-echo "[Step 3] Job sent (Plot Dashboard). ID: $JOB3 (Depends on $JOB2)"
+JOB2=$(sbatch --parsable --dependency=afterany:$JOB1 "$PLOT_MET_SCRIPT" "$REPO_ROOT" "$OUT_DIR")
+echo "[Step 2] Job sent (Plot Dashboard).           ID: $JOB2 (Depends on $JOB1)"
 
+# Plotting the images and spectra
+JOB3=$(sbatch --parsable --dependency=afterany:$JOB1 "$PLOT_IMG_SCRIPT" "$REPO_ROOT" "$OUT_DIR" "$DATA_DIR")
+echo "[Step 3] Job sent (Plot Images & Spectra).    ID: $JOB3 (Depends on $JOB1)"
+
+
+#--- PART 2: Second Training Session ---#
+
+# Second part of the training from RESUME
+JOB4=$(sbatch --parsable --dependency=afterok:$JOB1 "$TRAIN_SCRIPT" "$REPO_ROOT" "$OUT_DIR" "$DATA_DIR" resume all)
+echo "[Step 4] Job sent (Train Resume).             ID: $JOB4 (Depends on $JOB1)"
+
+# Plotting the metrics
+JOB5=$(sbatch --parsable --dependency=afterany:$JOB4 "$PLOT_MET_SCRIPT" "$REPO_ROOT" "$OUT_DIR")
+echo "[Step 5] Job sent (Plot Dashboard).           ID: $JOB5 (Depends on $JOB4)"
+
+# Plotting the images and spectra
+JOB6=$(sbatch --parsable --dependency=afterany:$JOB4 "$PLOT_IMG_SCRIPT" "$REPO_ROOT" "$OUT_DIR" "$DATA_DIR")
+echo "[Step 6] Job sent (Plot Images & Spectra).    ID: $JOB6 (Depends on $JOB4)"
+
+
+
+
+# Waiting for resources
 sleep 10
 
 #--- JOB INFORMATION ---#
--- slurm/train_astropt_multiGPU.sh
+++ slurm/train_astropt_multiGPU.sh
@@ -4,8 +4,8 @@
 #SBATCH --job-name=Train_AstroPT_DDP
 #SBATCH --partition=gpu
 #SBATCH --nodes=1                # Nodes number
-#SBATCH --ntasks=1               # Total task number
-#SBATCH --cpus-per-task=64       # CPUs for task
+#SBATCH --ntasks=1               # Total task number (1 task invoking torchrun)
+#SBATCH --cpus-per-task=64       # CPUs for task (16 per GPU * 4 GPUs)
 #SBATCH --gpus-per-task=4        # GPUs for task - DDP
 #SBATCH --mem=256G               # Requested RAM
 #SBATCH --time=12:00:00          # Requested time
@@ -14,7 +14,7 @@
 #SBATCH --output=logs/astropt_train_DDP_%j.out   # Output logs file
 #SBATCH --error=logs/astropt_train_DDP_%j.err    # Error logs file
 
-#--- Enviroment configuration ---#
+#--- Environment configuration ---#
 echo "------------------------------------------------------"
 echo "Starting the job $SLURM_JOB_ID on node $SLURM_NODELIST"
 echo "------------------------------------------------------"
@@ -29,14 +29,15 @@ echo "Changing directory to: $REPO_ROOT"
 cd "$REPO_ROOT" || exit 1
 
 # Activating AstroPT enviroment
-source  .venv/bin/activate
+source .venv/bin/activate
 
 # Exports cache
 export UV_CACHE_DIR="/home/valonso/iac18_mhuertas_shared/valonso/cache/uv_cache"
 export HF_HOME="/home/valonso/iac18_mhuertas_shared/valonso/cache/huggingface"
 
 # DDP optimization
-export OMP_NUM_THREADS=16
+# Matches cpus-per-task (64) / gpus (4) = 16
+export OMP_NUM_THREADS=16 
 
 # Arguments
 # Output Dir (Required):
@@ -46,19 +47,25 @@ OUT_DIR=${1:-"logs/astropt_100M_arrow"}
 DATA_DIR=${2:-"/home/valonso/iac18_aasensio_shared/euclid_dr1/processed_data_arrow"}
 
 # Training Mode (Optional): Default resume
-MODE=${3:-resume} 
+TRAIN_MODE=${3:-resume}
+
+# Saving checkpoint mode (Optional): Default both
+CHECK_MODE=${4:-both}
 
 # Running the training
 echo "Training AstroPT in DDP GPU:"
-echo "   OUT DIR:   $OUT_DIR"
-echo "   DATA DIR:  $DATA_DIR"
-echo "   MODE:      $MODE"
+echo "  OUT DIR:       $OUT_DIR"
+echo "  DATA DIR:      $DATA_DIR"
+echo "  TRAIN MODE:    $TRAIN_MODE"
+echo "  CHECKPOINT:    $CHECK_MODE"
 
 # Running Python Script with torch
+# Note: --max_run_hours is set slightly lower than SBATCH time to allow clean autosave
 torchrun --standalone --nproc_per_node=4 scripts/train_multimodal_arrow.py \
     --out_dir "$OUT_DIR" \
     --data_dir "$DATA_DIR" \
-    --init_from $MODE \
+    --init_from "$TRAIN_MODE" \
+    --checkpoint_save_type "$CHECK_MODE" \
     --max_run_hours "11:55:00"
 
 echo "------------------------------------------------------"
