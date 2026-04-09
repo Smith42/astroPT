dcd044fd7b0652220268ff2b6fbc8776367c7762
Victor Alonso Rodriguez <victoralonsorodriguez61@gmail.com>
2026-04-09 23:14:30 +0100

    feat: scheduler training launcher and time extension for each training part

---
+++ slurm/delayed_launch.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+TARGET_DATE="2026-04-10 00:01:00"
+COMMAND_TO_RUN="./astroPT/slurm/launch_train_astropt_multiGPU.sh -n 'TokMixRan + CrossRecLoss' -d 'First training with a token mixing with random block size. First training with a cross-rconstruction loss added to the usual train loss. This configuration expects to improve the cross reconstruction and the probing tasks forcing the modal to learn the cross modality physics'"
+
+# Convert dates to seconds since epoch
+TARGET_TIME=$(date -d "$TARGET_DATE" +%s)
+CURRENT_TIME=$(date +%s)
+
+# Calculate remaining seconds
+SLEEP_TIME=$((TARGET_TIME - CURRENT_TIME))
+
+if [ "$SLEEP_TIME" -le 0 ]; then
+    echo "[!] The target date has already passed. Executing immediately..."
+    eval "$COMMAND_TO_RUN"
+else
+    # Display formatted wait time (optional)
+    HOURS=$((SLEEP_TIME / 3600))
+    MINS=$(((SLEEP_TIME % 3600) / 60))
+    
+    echo "-----------------------------------------------------------------"
+    echo "⏳ Waiting $HOURS hours and $MINS minutes (until $TARGET_DATE)..."
+    echo "Scheduled command: $COMMAND_TO_RUN"
+    echo "-----------------------------------------------------------------"
+    
+    # Pause execution
+    sleep "$SLEEP_TIME"
+    
+    echo "▶️ [$(date)] Executing command..."
+    eval "$COMMAND_TO_RUN"
+fi
+
